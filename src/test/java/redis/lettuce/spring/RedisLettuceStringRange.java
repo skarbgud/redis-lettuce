8a73fdd411d28fdc560b4f1a91d0654a7384e165
GyuHyeong Nam <sjarbgud@gmail.com>
2026-02-13 21:07:46 +0900

    RedisClient 연결 중복코드 제거 (템플릿 패턴 적용)

---
+++ src/test/java/redis/lettuce/spring/RedisLettuceStringRange.java
@@ -0,0 +1,22 @@
+package redis.lettuce.spring;
+
+import org.junit.jupiter.api.Test;
+import redis.lettuce.CommandAction;
+import redis.lettuce.CommandTemplate;
+
+public class RedisLettuceStringRange {
+
+    @Test
+    public void substring() {
+        CommandAction action = (redisCommands -> {
+            String key = "lettuce:string";
+            String value = "hello";
+
+            redisCommands.set(key, value);
+
+            String get = redisCommands.get(key);
+            System.out.println("get = " + get);
+        });
+        CommandTemplate.commandAction(action);
+    }
+}
